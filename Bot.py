5672fe40d07aff11e6e06448fb4fb3175d3285c6
Kuptuk <49812392+Kuptuk@users.noreply.github.com>
2019-08-12 18:24:25 +0300

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,13 @@
+#Импорт модулей
+import telebot
+
+#Связь с токеном и создание бота
+bot = telebot.TeleBot("950234764:AAF8m_8vpMothwPryFNhbFka1F8s-MHTpE0")
+
+#/start
+@bot.message_handler(commands=["start"])
+def start_message(message):
+    bot.send_message(message.chat.id, "Я в работе!")
+
+#Работа бота
+bot.polling()
