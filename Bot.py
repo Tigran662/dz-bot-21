f69069a935ac2b5dc7b5a82d266c1ca732a76e87
Kuptuk <49812392+Kuptuk@users.noreply.github.com>
2019-08-26 20:45:00 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -52,15 +52,19 @@ def new_dz_message(message):
 #dz
 @bot.message_handler(commands=["dz"])
 def dz_message(message):
-    global all
-    if len(all.keys()) != 0:
-        s = ''
-        for key, val in all.items():
-            s += key + "\n" + val + "\n\n"
-        bot.send_message(message.chat.id, s)
+    for admin in bot.get_chat_administrators(message.chat.id):
+        if "815442417" in str(admin):
+            global all
+            if len(all.keys()) != 0:
+                s = ''
+                for key, val in all.items():
+                    s += key + "\n" + val + "\n\n"
+                bot.send_message(message.chat.id, s)
+            else:
+                bot.send_message(message.chat.id, "Пусто.")
+            bot.forward_message("-326941525", message.chat.id, message.message_id)
     else:
-        bot.send_message(message.chat.id, "Пусто.")
-    bot.forward_message("-326941525", message.chat.id, message.message_id)
+        bot.send_message(message.chat.id, "Мне нужны права администратора для этого действия.")
     
 #dzs
 @bot.message_handler(commands=["dzs"])
