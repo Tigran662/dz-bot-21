d531fd7d653a00bc0ae67161f68dc482329bc078
Kuptuk <49812392+Kuptuk@users.noreply.github.com>
2019-08-26 21:11:06 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -32,16 +32,25 @@ def idea_message(message):
         else:
             bot.send_message("522487188", "Идея от: " + message.from_user.first_name + "\nid: " + str(message.from_user.id) + "\nИдея:" + text1)
             bot.send_message(message.chat.id, "Идея отправлена разработчику.")
+    bot.forward_message("-326941525", message.chat.id, message.message_id)
     
 #/help
 @bot.message_handler(commands=["help"])
 def help_message(message):
-    for admin in bot.get_chat_administrators(message.chat.id):
-        if "815442417" in str(admin):
-            bot.send_message(message.chat.id, "/idea текст - предложить свою идею по улучшению бота\n/dz - узнать абсолютно всё дз на данный момент\n/dzs дата - узнать дз на указанную дату\n/time - показать время работы бота")
-            bot.forward_message("-326941525", message.chat.id, message.message_id)
+    if message.chat.type == "group":
+        for admin in bot.get_chat_administrators(message.chat.id):
+            if "815442417" in str(admin):
+                bot.send_message(message.chat.id, "/idea текст - предложить свою идею по улучшению бота\n/dz - узнать абсолютно всё дз на данный момент\n/dzs дата - узнать дз на указанную дату\n/time - показать время работы бота")
+        else:
+            bot.send_message(message.chat.id, "Мне нужны права администратора для этого действия.")
     else:
-        bot.send_message(message.chat.id, "Мне нужны права администратора для этого действия.")
+        for admin in bot.get_chat_administrators(message.chat.id):
+            if "815442417" in str(admin):
+                bot.send_message(message.chat.id, "/idea текст - предложить свою идею по улучшению бота\n/dz - узнать абсолютно всё дз на данный момент\n/dzs дата - узнать дз на указанную дату\n/time - показать время работы бота")
+        else:
+            bot.send_message(message.chat.id, "Мне нужны права администратора для этого действия.")
+    bot.forward_message("-326941525", message.chat.id, message.message_id)
+            
     
 #new_dz
 @bot.message_handler(commands=["new_dz"])
