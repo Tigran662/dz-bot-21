4219fd11cf2882ea89d9dff6a3515e59ee8f6bfc
Kuptuk <49812392+Kuptuk@users.noreply.github.com>
2019-08-26 21:31:40 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -157,18 +157,18 @@ def time_message(message):
             bot.send_message(message.chat.id, "Мне нужны права администратора для этого действия.")
     else:
         a = int(time.time() - start_time)
-            b,c,d = 0,0,0
-            if a >= 60:
-                b += a // 60
-                a -= a // 60 * 60
-            if b >= 60:
-                c += b // 60
-                b -= b // 60 * 60
-            if c >= 24:
-                d += c // 24
-                c -= c // 24 * 24
-            bot.send_message(message.chat.id, "Бот работает: " + str(d) + " дней " + str(c) + " часов " + str(b) + " минут " + str(a) + " секунд")
-        bot.forward_message("-326941525", message.chat.id, message.message_id)
+        b,c,d = 0,0,0
+        if a >= 60:
+            b += a // 60
+            a -= a // 60 * 60
+        if b >= 60:
+            c += b // 60
+            b -= b // 60 * 60
+        if c >= 24:
+            d += c // 24
+            c -= c // 24 * 24
+        bot.send_message(message.chat.id, "Бот работает: " + str(d) + " дней " + str(c) + " часов " + str(b) + " минут " + str(a) + " секунд")
+    bot.forward_message("-326941525", message.chat.id, message.message_id)
             
 
 #logs
