80f21d1fbd7a3b04ce4446d2e9f1acd3e36249f6
Kuptuk <49812392+Kuptuk@users.noreply.github.com>
2019-08-26 21:41:52 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -41,6 +41,7 @@ def help_message(message):
         for admin in bot.get_chat_administrators(message.chat.id):
             if "815442417" in str(admin):
                 bot.send_message(message.chat.id, "/idea текст - предложить свою идею по улучшению бота\n/dz - узнать абсолютно всё дз на данный момент\n/dzs дата - узнать дз на указанную дату\n/time - показать время работы бота")
+                break
         else:
             bot.send_message(message.chat.id, "Мне нужны права администратора для этого действия.")
     else:
