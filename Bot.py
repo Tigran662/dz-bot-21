dcf3630bfea8905502ec803b5a31c06e07e158fa
Kuptuk <49812392+Kuptuk@users.noreply.github.com>
2019-09-15 21:54:37 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,258 +1,79 @@
-#Импорт модулей
 import telebot
-import time
+import pymongo
+from telebot import types
 import os
 
-#Связь с токеном и создание бота
-token = os.environ.get("TOKEN")
-bot = telebot.TeleBot(token)
+mm = os.environ.get("Mongo")
+tt = os.environ.get("TOKEN")
+my_client = pymongo.MongoClient(mm)
+bot = telebot.TeleBot(tt)
 
-start_time = time.time()
+my_database = my_client.dz
+my_collection = my_database.dz
 
-all = {"03.09.2019":"1. Алгебра: тонкий сборник стр 79 тест 1. Стр 83 1-6","10.09.2019":"1. Русский язык: И.Бунин 'Легкое дыхание'. Стр 16 упр 1 продолжить рассказ. Стр 16 упр 8 \n2. Украинский язык: Довідник тести 16-24 на стр 261-262. Підручник стр 133 вправа 280"}
-rasps = {"Вторник":"1. Русский язык (88)\n2. Украинский язык (33)\n3. Математика (82)\n4. География (82)","Среда":"1. Информатика (30/33)\n2.Биология (72)\n3. Физика (92)\n4. Физ-ра"}
-
-#/idea
-@bot.message_handler(commands=["idea"])
-def idea_message(message):
-    if message.chat.type == "group":
-        for admin in bot.get_chat_administrators(message.chat.id):
-            if "950234764" in str(admin):
-                text1 = message.text[5::]
-                if text1 == '':
-                    bot.send_message(message.chat.id, "Возникла ошибка.\nВозможная причина: Отсутствует текст.")
-                else:
-                    bot.send_message("522487188", "Идея от: " + message.from_user.first_name + "\nid: " + str(message.from_user.id) + "\nИдея:" + text1)
-                    bot.send_message(message.chat.id, "Идея отправлена разработчику.")
-                break
-        else:
-            bot.send_message(message.chat.id, "Мне нужны права администратора для этого действия.")
-    else:
-        text1 = message.text[5::]
-        if text1 == '':
-            bot.send_message(message.chat.id, "Возникла ошибка.\nВозможная причина: Отсутствует текст.")
-        else:
-            bot.send_message("522487188", "Идея от: " + message.from_user.first_name + "\nid: " + str(message.from_user.id) + "\nИдея:" + text1)
-            bot.send_message(message.chat.id, "Идея отправлена разработчику.")
-    bot.forward_message("-326941525", message.chat.id, message.message_id)
-
-#/bug
-@bot.message_handler(commands=["bug"])
-def bug_message(message):
-    if message.chat.type == "group":
-        for admin in bot.get_chat_administrators(message.chat.id):
-            if "950234764" in str(admin):
-                text1 = message.text[4::]
-                if text1 == '':
-                    bot.send_message(message.chat.id, "Возникла ошибка.\nВозможная причина: Отсутствует текст.")
-                else:
-                    bot.send_message("522487188", "Ошибка от: " + message.from_user.first_name + "\nid: " + str(message.from_user.id) + "\nСуть ошибки:" + text1)
-                    bot.send_message(message.chat.id, "Теперь разработчик в курсе этой проблемы.")
-                break
-        else:
-            bot.send_message(message.chat.id, "Мне нужны права администратора для этого действия.")
-    else:
-        text1 = message.text[5::]
-        if text1 == '':
-            bot.send_message(message.chat.id, "Возникла ошибка.\nВозможная причина: Отсутствует текст.")
-        else:
-            bot.send_message("522487188", "Ошибка от: " + message.from_user.first_name + "\nid: " + str(message.from_user.id) + "\nСуть ошибки:" + text1)
-            bot.send_message(message.chat.id, "Теперь разработчик в курсе этой проблемы.")
-    bot.forward_message("-326941525", message.chat.id, message.message_id)
-    
-#/help
-@bot.message_handler(commands=["help"])
-def help_message(message):
-    if message.chat.type == "group":
-        for admin in bot.get_chat_administrators(message.chat.id):
-            if "950234764" in str(admin):
-                bot.send_message(message.chat.id, "/idea текст - предложить свою идею по улучшению бота\n/bug - сообщить об ошибке\n/dz - узнать абсолютно всё дз на данный момент\n/dzs дата - узнать дз на указанную дату\n/time - показать время работы бота\n/rasp - показать расписание на неделю\n/help_developers - команды для проверенных людей")
-                break
-        else:
-            bot.send_message(message.chat.id, "Мне нужны права администратора для этого действия.")
-    else:
-        bot.send_message(message.chat.id, "/idea текст - предложить свою идею по улучшению бота\n/bug - сообщить об ошибке\n/dz - узнать абсолютно всё дз на данный момент\n/dzs дата - узнать дз на указанную дату\n/time - показать время работы бота\n/rasp - показать расписание на неделю\n/help_developers - команды для проверенных людей")
-    bot.forward_message("-326941525", message.chat.id, message.message_id)
-    
-#/help_developers
-@bot.message_handler(commands=["help_developers"])
-def help_developers_message(message):
-    if message.chat.type == "group":
-        for admin in bot.get_chat_administrators(message.chat.id):
-            if "950234764" in str(admin):
-                if message.from_user.id == 522487188:
-                    bot.send_message(message.chat.id, "/new_dz дата текст - создать новое дз\n/delete_dz дата - удалить дз на определённую дату")
+@bot.message_handler(commands=["newdz"])
+def newdz_message(message):
+    if message.from_user.id == 522487188:
+        date = message.text[7:21]
+        text = message.text[22::]
+        if date != "" and text != "":
+            my_cursor = my_collection.find()
+            for item in my_cursor:
+                if item["date"] == date:
+                    my_collection.update_one({"text":item["text"]},{"$set":{"text":item["text"]+"\n"+"• " + text + "\n"}})
+                    bot.send_message(message.chat.id, "Дз обновлено")
                     break
-                else:
-                    bot.send_message(message.chat.id, "У вас нет прав.")
-                    break
+            else:
+                my_collection.insert_one({"date": date, "text": "• " + text + "\n"})
+                bot.send_message(message.chat.id, "Дз создано")
         else:
-            bot.send_message(message.chat.id, "Мне нужны права администратора для этого действия.")
+            bot.send_message(message.chat.id, "Дата или текст указаны неправильно")
     else:
-        if message.from_user.id == 522487188:
-            bot.send_message(message.chat.id, "/new_dz дата текст - создать новое дз\n/delete_dz дата - удалить дз на определённую дату")
-        else:
-            bot.send_message(message.chat.id, "У вас нет прав.")
-    bot.forward_message("-326941525", message.chat.id, message.message_id)
-    
-            
-    
-#new_dz
-@bot.message_handler(commands=["new_dz"])
-def new_dz_message(message):
-    global all
-    if message.from_user.id != 522487188:
         bot.send_message(message.chat.id, "У вас нет прав.")
-    else:
-        date = message.text[8:18]
-        text = message.text[19::]
-        if date != '' and text != '':
-            all.update({date:text})
-            bot.send_message(message.chat.id, "Дз добавлено!\n" + "Дата: " + date + "\nСодержание: " + text)
-            bot.send_message("-366936457", "Дз обновилось!\n" + str(all))
-        else:
-            bot.send_message(message.chat.id, "Возникла ошибка.\nВозможные причины: Отсутствует дата или текст.")
-    bot.forward_message("-326941525", message.chat.id, message.message_id)
 
-#dz
 @bot.message_handler(commands=["dz"])
-def dz_message(message):
-    global all
-    if message.chat.type == "group":
-        for admin in bot.get_chat_administrators(message.chat.id):
-            if "950234764" in str(admin):
-                if len(all.keys()) != 0:
-                    s = ''
-                    for key, val in all.items():
-                        s += key + "\n" + val + "\n\n"
-                    bot.send_message(message.chat.id, s)
-                else:
-                    bot.send_message(message.chat.id, "Пусто.")
-                break
-        else:
-            bot.send_message(message.chat.id, "Мне нужны права администратора для этого действия.")
-    else:
-        if len(all.keys()) != 0:
-            s = ''
-            for key, val in all.items():
-                s += key + "\n" + val + "\n\n"
-            bot.send_message(message.chat.id, s)
-        else:
-            bot.send_message(message.chat.id, "Пусто.")
-    bot.forward_message("-326941525", message.chat.id, message.message_id)
-   
-#rasp
-@bot.message_handler(commands=["rasp"])
-def rasp_message(message):
-    global rasps
-    if message.chat.type == "group":
-        for admin in bot.get_chat_administrators(message.chat.id):
-                if "950234764" in str(admin):
-                    s = ''
-                    for key, val in rasps.items():
-                        s += key + "\n" + val + "\n\n"
-                    bot.send_message(message.chat.id, s)
-                    break
-        else:
-            bot.send_message(message.chat.id, "Мне нужны права администратора для этого действия.")
-    else:
-        s = ''
-        for key, val in rasps.items():
-            s += key + "\n" + val + "\n\n"
-        bot.send_message(message.chat.id, s)
-    bot.forward_message("-326941525", message.chat.id, message.message_id)
-         
-#dzs
-@bot.message_handler(commands=["dzs"])
-def dzs_message(message):
-    global all
-    if message.chat.type == "group":
-        for admin in bot.get_chat_administrators(message.chat.id):
-            if "950234764" in str(admin):
-                date = message.text[5::]
-                if date != '':
-                    if date in all.keys():
-                        bot.send_message(message.chat.id, all.get(date))
-                    else:
-                        bot.send_message(message.chat.id, "Ошибка в дате.")
-                else:
-                    bot.send_message(message.chat.id, "Возникла ошибка.\nВозможная причина: Отсутствует дата.")
-                break
-        else:
-            bot.send_message(message.chat.id, "Мне нужны права администратора для этого действия.")
-    else:
-        date = message.text[5::]
-        if date != '':
-            if date in all.keys():
-                bot.send_message(message.chat.id, all.get(date))
-            else:
-                bot.send_message(message.chat.id, "Даты не существует.")
-        else:
-            bot.send_message(message.chat.id, "Возникла ошибка.\nВозможная причина: Отсутствует дата.")
-    bot.forward_message("-326941525", message.chat.id, message.message_id)
-    
+def url(message):
+    my_cursor = my_collection.find()
+    markup = types.InlineKeyboardMarkup()
+    a = dict()
+    for item in my_cursor:
+        a.update({item["date"]:item["text"]})
+    d1 = dict(sorted(a.items(), key = lambda x:x[0]))
+    for key in d1.keys():
+        btn_my_site=types.InlineKeyboardButton(text=str(key),callback_data=item["date"])
+        markup.add(btn_my_site)
+    bot.send_message(message.chat.id, "Выберите дату", reply_markup = markup)
 
-#delete_dz
-@bot.message_handler(commands=["delete_dz"])
-def delete_dz_message(message):
-    global all
-    if message.from_user.id != 522487188:
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    my_cursor = my_collection.find()
+    for item in my_cursor:
+        if call.message:
+            if call.data == item["date"]:
+                bot.send_message(call.message.chat.id, item["date"] + "\n\n" + item["text"])
+
+@bot.message_handler(commands=["deletedz"])
+def deletedz_message(message):
+    if message.from_user.id == 522487188:
+        a = message.text.split()
+        my_cursor = my_collection.find()
+        for item in my_cursor:
+            if item["date"] == a[1]:
+                my_collection.delete_one({"date":item["date"]})
+                bot.send_message(message.chat.id, "Дз на дату " + a[1] + " удалено")
+                break
+        else:
+            bot.send_message(message.chat.id, "Такой даты нет")
+    else:
         bot.send_message(message.chat.id, "У вас нет прав.")
-    else:
-        date = message.text[11::]
-        if date != '':
-            if date in all.keys():
-                all.pop(date)
-                bot.send_message(message.chat.id, date + " осталось без дз(")
-                bot.send_message("-366936457", "Дз обновилось!\n" + str(all))
-            else:
-                bot.send_message(message.chat.id, "Даты не существует.")
-        else:
-            bot.send_message(message.chat.id, "Возникла ошибка.\nВозможная причина: Отсутствует дата.")
-    bot.forward_message("-326941525", message.chat.id, message.message_id)
-            
-#time
-@bot.message_handler(commands=["time"])
-def time_message(message):
-    global start_time
-    if message.chat.type == "group":
-        for admin in bot.get_chat_administrators(message.chat.id):
-            if "950234764" in str(admin):
-                a = int(time.time() - start_time)
-                b,c,d = 0,0,0
-                if a >= 60:
-                    b += a // 60
-                    a -= a // 60 * 60
-                if b >= 60:
-                    c += b // 60
-                    b -= b // 60 * 60
-                if c >= 24:
-                    d += c // 24
-                    c -= c // 24 * 24
-                bot.send_message(message.chat.id, "Бот работает: " + str(d) + " дней " + str(c) + " часов " + str(b) + " минут " + str(a) + " секунд")
-                break
-        else:
-            bot.send_message(message.chat.id, "Мне нужны права администратора для этого действия.")
-    else:
-        a = int(time.time() - start_time)
-        b,c,d = 0,0,0
-        if a >= 60:
-            b += a // 60
-            a -= a // 60 * 60
-        if b >= 60:
-            c += b // 60
-            b -= b // 60 * 60
-        if c >= 24:
-            d += c // 24
-            c -= c // 24 * 24
-        bot.send_message(message.chat.id, "Бот работает: " + str(d) + " дней " + str(c) + " часов " + str(b) + " минут " + str(a) + " секунд")
-    bot.forward_message("-326941525", message.chat.id, message.message_id)
-            
 
-#logs
+@bot.message_handler(commands=["send"])
+def send1_message(message):
+    if message.from_user.id == 522487188:
+        bot.send_message(-1001219015757, message.text[6::])
+
 @bot.message_handler(content_types=["text"])
 def text_message(message):
-    bot.forward_message("-326941525", message.chat.id, message.message_id)
-    
-#Работа бота
+    bot.send_message(-326941525, "Даунич с именем " + message.from_user.first_name + " написал " + message.text)
+
 bot.polling()
