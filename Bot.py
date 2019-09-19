b0857ffcb5192a17b4a5e002ecf76d9526bb29c1
Kuptuk <49812392+Kuptuk@users.noreply.github.com>
2019-09-19 06:41:35 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -18,7 +18,7 @@ my_collection2 = my_database2.rasp
 def start_message(message):
     bot.send_message(message.chat.id, "/dz - узнать дз\n/rasp - показать расписание")
     bot.send_message(-326941525, "Гавночух унитаза по имени " + message.from_user.first_name + " написал " + message.text) 
-    
+
 @bot.message_handler(commands=["newdz"])
 def newdz_message(message):
     if message.from_user.id == 522487188:
@@ -48,26 +48,23 @@ def url(message):
         a.update({item["date"]:item["text"]})
     d1 = dict(sorted(a.items(), key = lambda x:x[0]))
     for key in d1.keys():
-        btn_my_site=types.InlineKeyboardButton(text=str(key),callback_data=key)
+        btn_my_site=types.InlineKeyboardButton(text=str(key),callback_data=item["date"])
         markup.add(btn_my_site)
     bot.send_message(message.chat.id, "Выберите дату", reply_markup = markup)
-    bot.send_message(-326941525, "Такому дерьму, как " + message.from_user.first_name + " понадобилось дз, вот же чмо)")
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
-    name = call.from_user.first_name
     my_cursor = my_collection.find()
     for item in my_cursor:
         if call.message:
             if call.data == item["date"]:
                 bot.send_message(call.message.chat.id, item["date"] + "\n\n" + item["text"])
-                bot.send_message(-326941525, name + " узналo дз на " + call.data + ", наверное это чмо его не записало, в прочем, ничё нового =/")
     my_cursor = my_collection2.find()
     for item in my_cursor:
         if call.message:
             if call.data == item["idch"]:
                 bot.send_message(call.message.chat.id, "• " + item["ch"] + "\n\n" + item["rasp"])
-                bot.send_message(-326941525, name + " узналo расписание на " + call.data + ", наверное это чмо его не записало, в прочем, ничё нового =/")
+
 
 @bot.message_handler(commands=["deletedz"])
 def deletedz_message(message):
@@ -88,9 +85,25 @@ def deletedz_message(message):
 def send1_message(message):
     if message.from_user.id == 522487188:
         bot.send_message(-1001219015757, message.text[6::])
-        
+
+@bot.message_handler(commands=["set"])
+def set_message(message):
+    if message.from_user.id == 522487188:
+        my_cursor = my_collection2.find()
+        for item in my_cursor:
+            if item["idch"] == "prosto":
+                if item["val"] == "Числитель":
+                    my_collection2.update_one({"idch":"prosto"},{"$set":{"val":"Знаменатель"}})
+                    bot.send_message(message.chat.id,"Изменено на знаменатель")
+                    break
+                else:
+                    my_collection2.update_one({"idch":"prosto"},{"$set":{"val":"Числитель"}})
+                    bot.send_message(message.chat.id,"Изменено на числитель")
+                    break
+
 @bot.message_handler(commands=["rasp"])
 def rasp_message(message):
+    my_cursor = my_collection2.find()
     markup2 = types.InlineKeyboardMarkup()
     btn_my_site=types.InlineKeyboardButton(text="Понедельник",callback_data="Mon")
     markup2.add(btn_my_site)
@@ -102,7 +115,9 @@ def rasp_message(message):
     markup2.add(btn_my_site)
     btn_my_site=types.InlineKeyboardButton(text="Пятница",callback_data="Fri")
     markup2.add(btn_my_site)
-    bot.send_message(message.chat.id, "Выберите день недели", reply_markup = markup2)
+    for item in my_cursor:
+        if item["idch"] == "prosto":
+            bot.send_message(message.chat.id, "Выберите день недели\nНа данный момент: " + item["val"], reply_markup = markup2)
 
 @bot.message_handler(content_types=["text"])
 def text_message(message):
