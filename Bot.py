1226656c4a8170462888b30369fd68238ed1ac3f
Kuptuk <49812392+Kuptuk@users.noreply.github.com>
2019-09-27 11:11:50 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -15,17 +15,21 @@ my_collection = my_database.dz
 my_database2 = my_client.rasp
 my_collection2 = my_database2.rasp
 
+my_collection3 = my_database2.stat
+
 @bot.message_handler(commands=["start"])
 def start_message(message):
-    bot.send_message(message.chat.id, "/dz - узнать дз\n/rasp - показать расписание")
+    bot.send_message(message.chat.id, "/dz - узнать дз\n/rasp - показать расписание\n/stat - статистика\n/bug текст - сообщить о баге (сюда можно и кидать идеи)")
     bot.send_message(-326941525, message.from_user.first_name + ": " + message.text)
 
 @bot.message_handler(commands=["newdz"])
 def newdz_message(message):
-    if message.from_user.id == 522487188:
-        date = message.text[7:21]
-        text = message.text[22::]
-        if date != "" and text != "":
+    try:
+        if message.from_user.id == 522487188:
+            a = message.text.split()
+            dnd = a[1]
+            date = a[2]
+            text = " ".join(a[3:])
             my_cursor = my_collection.find()
             for item in my_cursor:
                 if item["date"] == date:
@@ -33,12 +37,12 @@ def newdz_message(message):
                     bot.send_message(message.chat.id, "Дз обновлено")
                     break
             else:
-                my_collection.insert_one({"date": date, "text": "• " + text + "\n"})
+                my_collection.insert_one({"date": dnd + " " + date, "text": "• " + text + "\n"})
                 bot.send_message(message.chat.id, "Дз создано")
         else:
-            bot.send_message(message.chat.id, "Дата или текст указаны неправильно")
-    else:
-        bot.send_message(message.chat.id, "У вас нет прав.")
+            bot.send_message(message.chat.id, "У вас нет прав.")
+    except:
+        bot.send_message(message.chat.id,"Возникла ошибка!")
 
 @bot.message_handler(commands=["dz"])
 def url(message):
@@ -65,6 +69,31 @@ def callback_inline(call):
             if call.data == item["date"]:
                 try:
                     bot.send_message(call.from_user.id, item["date"] + "\n\n" + item["text"])
+                    my_cursor2 = my_collection3.find()
+                    for item2 in my_cursor2:
+                        if item2["id"] == str(call.from_user.id):
+                            if "Понедельник" in item["date"]:
+                                my_collection3.update_one({"id":str(call.from_user.id)},{"$set":{"Понедельник: ": item2["Понедельник: "] + 1}})
+                            elif "Вторник" in item["date"]:
+                                my_collection3.update_one({"id":str(call.from_user.id)},{"$set":{"Вторник: ": item2["Вторник: "] + 1}})
+                            elif "Среда" in item["date"]:
+                                my_collection3.update_one({"id":str(call.from_user.id)},{"$set":{"Среда: ": item2["Среда: "] + 1}})
+                            elif "Четверг" in item["date"]:
+                                my_collection3.update_one({"id":str(call.from_user.id)},{"$set":{"Четверг: ": item2["Четверг: "] + 1}})
+                            elif "Пятница" in item["date"]:
+                                my_collection3.update_one({"id":str(call.from_user.id)},{"$set":{"Пятница: ": item2["Пятница: "] + 1}})
+                            break
+                    else:
+                        if "Понедельник" in item["date"]:
+                            my_collection3.insert_one({"id": str(call.from_user.id), "Понедельник: ": 1, "Вторник: ": 0, "Среда: ": 0, "Четверг: ": 0, "Пятница: ": 0})
+                        elif "Вторник" in item["date"]:
+                            my_collection3.insert_one({"id": str(call.from_user.id), "Понедельник: ": 0, "Вторник: ": 1, "Среда: ": 0, "Четверг: ": 0, "Пятница: ": 0})
+                        elif "Среда" in item["date"]:
+                            my_collection3.insert_one({"id": str(call.from_user.id), "Понедельник: ": 0, "Вторник: ": 0, "Среда: ": 1, "Четверг: ": 0, "Пятница: ": 0})
+                        elif "Четверг" in item["date"]:
+                            my_collection3.insert_one({"id": str(call.from_user.id), "Понедельник: ": 0, "Вторник: ": 0, "Среда: ": 0, "Четверг: ": 1, "Пятница: ": 0})
+                        elif "Пятница" in item["date"]:
+                            my_collection3.insert_one({"id": str(call.from_user.id), "Понедельник: ": 0, "Вторник: ": 0, "Среда: ": 0, "Четверг: ": 0, "Пятница: ": 1})
                 except:
                     bot.send_message(call.message.chat.id, call.from_user.first_name + ", напишите мне в личку /start и я смогу отправлять вам сообщения!")
     my_cursor = my_collection2.find()
@@ -79,28 +108,46 @@ def callback_inline(call):
 
 @bot.message_handler(commands=["deletedz"])
 def deletedz_message(message):
-    if message.from_user.id == 522487188:
-        a = message.text.split()
-        my_cursor = my_collection.find()
-        for item in my_cursor:
-            if item["date"] == a[1]:
-                my_collection.delete_one({"date":item["date"]})
-                bot.send_message(message.chat.id, "Дз на дату " + a[1] + " удалено")
-                break
+    try:
+        if message.from_user.id == 522487188:
+            a = message.text.split()
+            my_cursor = my_collection.find()
+            for item in my_cursor:
+                if item["date"] == a[1] + " " + a[2]:
+                    my_collection.delete_one({"date":item["date"]})
+                    bot.send_message(message.chat.id, "Дз на дату " + a[2] + " удалено")
+                    break
+            else:
+                bot.send_message(message.chat.id, "Такой даты нет")
         else:
-            bot.send_message(message.chat.id, "Такой даты нет")
+            bot.send_message(message.chat.id, "У вас нет прав.")
+    except:
+        bot.send_message(message.chat.id, "Возникла ошибка!")
+
+@bot.message_handler(commands=["stat"])
+def stat_message(message):
+    my_cursor2 = my_collection3.find()
+    for item in my_cursor2:
+        if item["id"] == str(message.from_user.id):
+            bot.send_message(message.from_user.id, "Понедельник: " + str(item["Понедельник: "]) + "\n" + "Вторник: " + str(item["Вторник: "]) + "\n" + "Среда: " + str(item["Среда: "]) + "\n" + "Четверг: " + str(item["Четверг: "]) + "\n" + "Пятница: " + str(item["Пятница: "]))
+            break
     else:
-        bot.send_message(message.chat.id, "У вас нет прав.")
+        my_collection3.insert_one({"id": str(message.from_user.id), "Понедельник: ": 0, "Вторник: ": 0, "Среда: ": 0, "Четверг: ": 0, "Пятница: ": 0})
+        bot.send_message(message.from_user.id, "Понедельник: 0\nВторник: 0\nСреда: 0\nЧетверг: 0\nПятница: 0")
+    bot.send_message(-326941525, message.from_user.first_name + ": " + message.text)
+
+@bot.message_handler(commands=["bug"])
+def bug_message(message):
+    try:
+        bot.send_message(-326941525, message.from_user.first_name + ": " + " ".join(message.text.split()[1:]))
+        bot.send_message(message.chat.id, "Успешно отправлено, спасибо!")
+    except:
+        bot.send_message(message.chat.id, "Ошибка")
 
 @bot.message_handler(commands=["send"])
 def send1_message(message):
     if message.from_user.id == 522487188:
         bot.send_message(-1001219015757, message.text[6::])
-
-@bot.message_handler(commands=["print"])
-def print_message(message):
-    if message.from_user.id == 522487188:
-        bot.send_message(408011007, message.text[7::])
 
 @bot.message_handler(commands=["sendkl"])
 def send1_message(message):
